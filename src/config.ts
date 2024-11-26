6fa2f9e4da58ee9c1083d1a650ff9a9a4ee3c9a5
root <root@ip-172-31-37-139.sa-east-1.compute.internal>
2024-11-26 11:31:20 +0000

    Cruz Roja Ver 1.0

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const MEDPLUM_BASE_URL = 'https://api.epa-bienestar.com.ar';
+export const MEDPLUM_PROJECT_ID = '215ab210-b327-445c-9796-4ce860ee6c4b';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '472653584585-fia39ab73fvskfgh80apthnvoll4298f.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6Lf2ynoqAAAAAFZHg72QePMKmF-QpUO5sLIsnwa5';
+export const MEDPLUM_REGISTER_ENABLED = 'true';
+export const WDS_SOCKET_PORT = '4174';
